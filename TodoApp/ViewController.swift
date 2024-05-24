a8b3840307aec108435788ee2f7cd0e7bdc7b10b
jjwon2149 <jjwon2149@gmail.com>
2024-05-24 10:05:30 +0900

    feat: add tableView, NavigationrightButton

--- TodoApp/ViewController.swift
+++ TodoApp/ViewController.swift
@@ -7,13 +7,84 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Todos {
+    var todoTitle: String
+    var todoisChecked: Bool
+    
+}
 
+class ViewController: UIViewController {
+    
+    var todos = [Todos]()
+    var tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.title = "Todo"
+        
+        let rightBarButtonItem = UIBarButtonItem(title: "", image: UIImage(systemName: "plus"), target: self, action: #selector(rightBarButtonTapped))
+        self.navigationItem.rightBarButtonItem = rightBarButtonItem
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "ToDoCell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
     }
+    
+    @objc func rightBarButtonTapped() {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "새 할일", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "할일 추가", style: .default) { (action) in
+            let newTodo = Todos(todoTitle: textField.text!, todoisChecked: false)
+            self.todos.append(newTodo)
+            print(self.todos)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "새 할일을 여기에 적어주세요"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+}
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        todos[indexPath.row].todoisChecked.toggle()
+        tableView.reloadData()
+    }
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell", for: indexPath)
+        let todo = todos[indexPath.row]
+        cell.selectionStyle = .none
+        cell.textLabel?.text = todo.todoTitle
+        cell.accessoryType = todo.todoisChecked ? .checkmark : .none
+        return cell
+    }
+    
+    
 }
 
